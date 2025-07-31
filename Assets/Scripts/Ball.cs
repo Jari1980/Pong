ecbb7d2122cda8d8c4a899a52eff09a4f541a06d
Jari <jari.tenhunen@outlook.com>
2025-07-31 10:46:55 +0200

    Ball moving, bouncing on walls and Squares, computer is chasing ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float speed = 150.0f;
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        AddStartingForce();
+    }
+
+    private void AddStartingForce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1 - 0f);
+
+        Vector2 direction = new Vector2(x, y);
+        _rigidBody.AddForce(direction * this.speed);
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        _rigidBody.AddForce(force);
+    }
+}
